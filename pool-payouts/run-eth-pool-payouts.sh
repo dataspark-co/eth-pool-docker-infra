e0011f0441bc6bdf427ec25a70a4fb000603768b
Valera Rozuvan <valera.rozuvan@gmail.com>
2018-03-12 13:35:39 +0200

    Adding payouts module.

---
+++ pool-payouts/run-eth-pool-payouts.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run --name eth-pool-payouts --net="host" -d eth-pool-payouts-image
---
+++ pool-payouts/stop-eth-pool-payouts.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker stop eth-pool-payouts
+docker rm eth-pool-payouts
